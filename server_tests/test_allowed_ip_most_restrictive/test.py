9fa4eeaa31f324c4691b5213fc0fb8d9f4ed6ca9
MRI <popovicimarian2311@gmail.com>
2025-06-18 14:21:57 +0000

    test

--- server_tests/test_allowed_ip_most_restrictive/test.py
+++ server_tests/test_allowed_ip_most_restrictive/test.py
@@ -21,26 +21,26 @@ def run_test(s: TestServer, c: CoreApi):
     # Using global ts and c from testlib
     response = s.get("/api/pets/",
                      headers={"X-Forwarded-For": "1.3.3.7"})
-    assert_response_code_is(response, 403)
-    assert_response_header_contains(response, "Content-Type", "text")
     assert_response_body_contains(
         response, " not allowed ")
+    assert_response_code_is(response, 403)
+    assert_response_header_contains(response, "Content-Type", "text")
 
     c.update_runtime_config_file(f("change_config_remove_allowed_ip.json"))
 
     response = s.get("/api/pets/",
                      headers={"X-Forwarded-For": "1.3.3.7"})
-    assert_response_code_is(response, 200)
     assert_response_body_contains(response, "[]")
+    assert_response_code_is(response, 200)
 
     c.update_runtime_config_file(f("start_config.json"))
 
     response = s.get("//api/pets/",
                      headers={"X-Forwarded-For": "1.3.3.7"})
-    assert_response_code_is(response, 403)
-    assert_response_header_contains(response, "Content-Type", "text")
     assert_response_body_contains(
         response, " not allowed ")
+    assert_response_code_is(response, 403)
+    assert_response_header_contains(response, "Content-Type", "text")
 
 
 if __name__ == "__main__":

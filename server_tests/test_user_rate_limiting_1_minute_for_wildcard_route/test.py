f61e377de331811cc1387110676798cde8374d65
MRI <popovicimarian2311@gmail.com>
2025-07-03 15:27:09 +0000

    + test_user_rate_limiting_1_minute_for_wildcard_route

---
+++ server_tests/test_user_rate_limiting_1_minute_for_wildcard_route/test.py
@@ -0,0 +1,45 @@
+import requests
+import time
+import sys
+from testlib import *
+from core_api import CoreApi
+import os
+
+'''
+1. Sets up the rate limiting config to 5 requests / minute for route '/'.
+2. Sends 5 requests to '/'. Checks that those requests are not blocked.
+3. Send another more 5 request to '/'. Checks that they all are rate limited.
+4. Sends 100 requests to another route '/tests'. Checks that those requests are not blocked.
+'''
+
+
+def f(config_file: str):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), config_file)
+
+
+def run_test(s: TestServer, c: CoreApi):
+    for i in range(5):
+        response = s.get(
+            f"/api/execute/{generate_random_string(10)}",  headers={"X-Forwarded-For": "2.16.53.5"})
+        assert_response_code_is(response, 200)
+
+    # sleep for 10 seconds
+    time.sleep(5)
+
+    for i in range(10):
+        response = s.get(
+            f"/api/execute/{generate_random_string(10)}", headers={"X-Forwarded-For": "2.16.53.5"})
+        if i < 5:
+            pass
+        else:
+            assert_response_code_is(response, 429)
+
+    for _ in range(100):
+        response = s.get(
+            "/api/pets/", headers={"X-Forwarded-For": "2.16.53.5"})
+        assert_response_code_is(response, 200)
+
+
+if __name__ == "__main__":
+    args, s, c = init_server_and_core()
+    run_test(s, c)

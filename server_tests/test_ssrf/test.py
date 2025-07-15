b62a40d4bbe7acc7e95a9c11c138536c91698c42
MRI <marian@aikido.dev>
2025-07-15 11:40:06 +0000

    Update SSRF test

--- server_tests/test_ssrf/test.py
+++ server_tests/test_ssrf/test.py
@@ -38,13 +38,13 @@ def check_ssrf(ip):
 
 def run_test(s: TestServer, c: CoreApi):
     
-    # check_ssrf_with_event(500, "expect_detection_blocked.json")
+    check_ssrf_with_event(500, "expect_detection_blocked.json")
 
-    # c.update_runtime_config_file("change_config_disable_blocking.json")
-    # check_ssrf_with_event(400, "expect_detection_not_blocked.json")
+    c.update_runtime_config_file("change_config_disable_blocking.json")
+    check_ssrf_with_event(400, "expect_detection_not_blocked.json")
 
-    # c.update_runtime_config_file("start_config.json")
-    # check_ssrf_with_event(500, "expect_detection_blocked.json")
+    c.update_runtime_config_file("start_config.json")
+    check_ssrf_with_event(500, "expect_detection_blocked.json")
 
     ips = [
         "http://127.0.0.1:9081",

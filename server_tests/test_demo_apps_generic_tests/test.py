fcf1a9233b97ca2d2635a6e7eed94b50ac0e3052
Marian Popovici <marain@aikido.dev>
2025-08-13 13:04:32 +0300

    add new tests

---
+++ server_tests/test_demo_apps_generic_tests/test.py
@@ -0,0 +1,47 @@
+
+from testlib import *
+from core_api import CoreApi
+import os
+
+
+'''
+1. Send a very big request to the server.
+2. Send an sql injection payload to see if the server it's still working.
+3. Check for bot blocking.
+'''
+
+
+def run_test(s: TestServer, c: CoreApi):
+    file_path = os.path.join(os.path.dirname(__file__), "test.json")
+    response = s.post("/api/create", json.load(open(file_path, 'r')))
+    assert_response_code_is(
+        response, 200, f"Expected 200 for /api/create {response.text}")
+
+    response = s.post(
+        "/api/create", {"name": "Malicious Pet', 'Gru from the Minions') --"})
+    assert_response_code_is(
+        response, 500, f"Expected 500 for /api/create {response.text}")
+
+    # ------ Bot blocking ------
+
+    response = s.get("/api/pets/", headers={
+        "User-Agent": "1234googlebot1234"})
+    assert_response_code_is(
+        response, 403, f"Expected 403 for / {response.text}")
+
+    # ------ user blocking ------
+
+    response = s.get("/api/pets/", headers={
+        "user": "123456"})
+    assert_response_code_is(
+        response, 200, f"Expected 200 for user 123456/ {response.text}")
+
+    response = s.get("/api/pets/", headers={
+        "user": "789"})
+    assert_response_code_is(
+        response, 403, f"Expected 403 for user 789/ {response.text}")
+
+
+if __name__ == "__main__":
+    args, s, c = init_server_and_core()
+    run_test(s, c)

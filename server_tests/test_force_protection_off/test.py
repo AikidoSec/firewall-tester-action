c272be619d7632873575d6f1e53642dad1ee54da
Popovici Marian <marian@aikido.dev>
2025-09-29 14:17:37 +0000

    Add tests for forceProtectionOff

---
+++ server_tests/test_force_protection_off/test.py
@@ -0,0 +1,40 @@
+from testlib import *
+from core_api import CoreApi
+
+
+'''
+1. Sets up a simple config and env AIKIDO_BLOCK=1
+2. Send some attacks and check that they are blocked.
+3. Update the config to disable blocking (forceProtectionOff: true) and check that the attacks are not blocked.
+4. Update the config to enable blocking, and check that the attacks are blocked.
+'''
+
+
+def check_shell_injection(response_code):
+    # shell injection
+    response = s.post("/api/execute", {"userCommand": "whoami"})
+    assert_response_code_is(response, response_code)
+
+    # sql injection
+    response = s.post(
+        "/api/create", {"name": "Malicious Pet', 'Gru from the Minions') --"})
+    assert_response_code_is(response, response_code)
+
+    # path traversal
+    response = s.get("/api/read?path=../secrets/key.txt")
+    assert_response_code_is(response, response_code)
+
+
+def run_test(s: TestServer, c: CoreApi):
+    check_shell_injection(500)
+
+    c.update_runtime_config_file("change_config_force_protection_off.json")
+    check_shell_injection(200)
+
+    c.update_runtime_config_file("start_config.json")
+    check_shell_injection(500)
+
+
+if __name__ == "__main__":
+    args, s, c = init_server_and_core()
+    run_test(s, c)

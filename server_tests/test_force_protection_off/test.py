8dfbf0023e6d2fa11af6deb952d14bab398b7c7d
Marian Popovici <marian@aikido.dev>
2025-11-17 14:32:20 +0200

    update test cases for force protection off (+ssrf)

--- server_tests/test_force_protection_off/test.py
+++ server_tests/test_force_protection_off/test.py
@@ -10,6 +10,14 @@ from core_api import CoreApi
 '''
 
 
+def start_mock_servers(target_container_name: str):
+    path = os.path.join(os.path.dirname(__file__), "mock-4000.sh")
+    subprocess.run(
+        f"docker run -d --name mock-4000-for-ssrf-{target_container_name} -v {path}:/mock-4000.sh:ro --network container:{target_container_name} alpine:3.20 sh /mock-4000.sh", shell=True)
+
+    time.sleep(20)
+
+
 def check_force_protection_off(response_code):
     # shell injection
     response = s.post("/api/execute", {"userCommand": "whoami"})
@@ -24,6 +32,11 @@ def check_force_protection_off(response_code):
     response = s.get("/api/read?path=../secrets/key.txt")
     assert_response_code_is(response, response_code, "path traversal")
 
+    # ssrf
+    response = s.post(
+        "/api/request", {"url": "http://127.0.0.1:4000"}, timeout=10)
+    assert_response_code_is(response, response_code, "ssrf")
+
 
 def run_test(s: TestServer, c: CoreApi):
     check_force_protection_off(500)
@@ -37,4 +50,9 @@ def run_test(s: TestServer, c: CoreApi):
 
 if __name__ == "__main__":
     args, s, c = init_server_and_core()
-    run_test(s, c)
+    try:
+        start_mock_servers("test_force_protection_off")
+        run_test(s, c)
+    finally:
+        subprocess.run(
+            f"docker rm -f mock-4000-for-ssrf-test_force_protection_off", shell=True)
